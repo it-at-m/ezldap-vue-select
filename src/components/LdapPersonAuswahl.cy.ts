9b38c336739a5aa6dae8ee24639277134f00c2c7
eidottermihi <eidottermihi@gmail.com>
2024-08-26 19:11:02 +0200

    WIP: add cypress for component tests

---
+++ src/components/LdapPersonAuswahl.cy.ts
@@ -0,0 +1,55 @@
+import LdapPersonAuswahl from "./LdapPersonAuswahl.vue";
+
+describe("<LdapPersonAuswahl />", () => {
+  it("renders", () => {
+    cy.intercept(
+      "GET",
+      "http://api.example.com/v1/ldap/search/findByUidWildcard?uid=*",
+      {
+        fixture: "ezldap-findByUidWildcard.json",
+      }
+    ).as("anyApiRequest");
+    cy.intercept("GET", "http://mucatar.example.com/avatar?uid=michaela*", {
+      fixture: "avatar.png,null",
+    }).as("mucatarRequestMichaela");
+    cy.intercept("GET", "http://mucatar.example.com/avatar?uid=amicha*", {
+      fixture: "avatar.png,null",
+    }).as("mucatarRequestAmicha");
+    // see: https://on.cypress.io/mounting-vue
+    cy.mount(LdapPersonAuswahl, {
+      props: {
+        label: "the custom label",
+        modelValue: undefined,
+        mucatarBaseUrl: "http://mucatar.example.com",
+        baseUrl: "http://api.example.com",
+      },
+    });
+
+    cy.get("#ldap-person-auswahl").type("mich");
+
+    cy.wait("@anyApiRequest");
+    cy.get(
+      '[data-testid="ldap-person-auswahl-amicha.miller"] .v-list-item-title'
+    ).contains("Amicha Miller");
+    cy.get(
+      '[data-testid="ldap-person-auswahl-amicha.miller"] .v-list-item-subtitle'
+    ).contains("ORG-A1-A12");
+    cy.get(
+      '[data-testid="ldap-person-auswahl-michaela.miller"] .v-list-item-title'
+    ).contains("Michaela Miller");
+    cy.get(
+      '[data-testid="ldap-person-auswahl-michaela.miller"] .v-list-item-subtitle'
+    ).contains("ORG-A2-A24");
+    cy.wait("@mucatarRequestMichaela");
+    cy.wait("@mucatarRequestAmicha");
+    cy.get(
+      '[data-testid="ldap-person-auswahl-michaela.miller"] .v-list-item__prepend'
+    )
+      .find("img")
+      .should(
+        "have.prop",
+        "src",
+        "http://mucatar.example.com/avatar?uid=michaela.miller&m=404"
+      );
+  });
+});
