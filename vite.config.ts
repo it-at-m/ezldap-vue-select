ae9e0668f96b19bcfd026642daf27888eb874ea1
langehm <38280183+langehm@users.noreply.github.com>
2026-04-22 16:14:21 +0200

    updated dependencies and adapted configs from refarch

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,47 @@
-import { resolve } from "path";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import vuetify from "vite-plugin-vuetify";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-    dts(),
-  ],
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/components/index.ts"),
-      name: "LdapPersonAuswahl",
-      // the proper extensions will be added
-      fileName: "ezldap-vue-select",
+export default defineConfig(() => {
+  const currentDir = dirname(fileURLToPath(import.meta.url));
+
+  return {
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+      }),
+      vuetify({
+        autoImport: true,
+      }),
+      dts({
+        tsconfigPath: "./tsconfig.app.json",
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["vue", "vuetify", "vuetify/lib"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue",
-          "vuetify/lib": "Vuetify",
+    build: {
+      lib: {
+        entry: resolve(currentDir, "src/components/index.ts"),
+        name: "LdapPersonAuswahl",
+        fileName: "ezldap-vue-select",
+      },
+      rollupOptions: {
+        external: ["vue", "vuetify", "vuetify/lib"],
+        output: {
+          globals: {
+            vue: "Vue",
+            "vuetify/lib": "Vuetify",
+          },
         },
       },
     },
-  },
+  };
 });
